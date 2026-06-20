1a84556f5a71d2ba4b3fc7e171c11629201fc1cf
Gus <amuwonge99@gmail.com>
2026-06-20 20:15:30 +0100

    added private subnet

--- infra/modules/vpc/outputs.tf
+++ infra/modules/vpc/outputs.tf
@@ -7,3 +7,8 @@ output "public_subnet_ids" {
   description = "Public subnet IDs"
   value       = aws_subnet.public[*].id
 }
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
