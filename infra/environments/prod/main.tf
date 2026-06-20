498aba373933dcdcd23e1d52ddacd2f8b43f9a36
Gus <amuwonge99@gmail.com>
2026-06-20 21:15:17 +0100

    added private subnet ids to ecs module

--- infra/environments/prod/main.tf
+++ infra/environments/prod/main.tf
@@ -23,14 +23,15 @@ module "alb" {
 }
 
 module "ecs" {
-  source            = "../../modules/ecs"
-  environment       = var.environment
-  vpc_id            = module.vpc.vpc_id
-  public_subnet_ids = module.vpc.public_subnet_ids
-  alb_sg_id         = module.alb.alb_sg_id
-  target_group_arn  = module.alb.target_group_arn
-  ecr_repo_url      = module.ecr.repository_url
-  image_tag         = var.image_tag
+  source             = "../../modules/ecs"
+  environment        = var.environment
+  vpc_id             = module.vpc.vpc_id
+  public_subnet_ids  = module.vpc.public_subnet_ids
+  private_subnet_ids = module.vpc.private_subnet_ids
+  alb_sg_id          = module.alb.alb_sg_id
+  target_group_arn   = module.alb.target_group_arn
+  ecr_repo_url       = module.ecr.repository_url
+  image_tag          = var.image_tag
 }
 
 module "dns" {
