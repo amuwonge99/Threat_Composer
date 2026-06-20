439327826acab2fa15d89546fdf4538c0c220c05
Gus <amuwonge99@gmail.com>
2026-06-20 23:13:10 +0100

    sample dev & test environments

---
+++ infra/environments/dev/main.tf
@@ -0,0 +1,45 @@
+module "vpc" {
+  source      = "../../modules/vpc"
+  environment = var.environment
+}
+
+module "ecr" {
+  source      = "../../modules/ecr"
+  environment = var.environment
+}
+
+module "acm" {
+  source      = "../../modules/acm"
+  domain_name = var.domain_name
+  subdomain   = var.subdomain
+}
+
+module "alb" {
+  source            = "../../modules/alb"
+  environment       = var.environment
+  vpc_id            = module.vpc.vpc_id
+  public_subnet_ids = module.vpc.public_subnet_ids
+  acm_cert_arn      = module.acm.certificate_arn
+}
+
+module "ecs" {
+  source             = "../../modules/ecs"
+  environment        = var.environment
+  vpc_id             = module.vpc.vpc_id
+  public_subnet_ids  = module.vpc.public_subnet_ids
+  private_subnet_ids = module.vpc.private_subnet_ids
+  alb_sg_id          = module.alb.alb_sg_id
+  target_group_arn   = module.alb.target_group_arn
+  ecr_repo_url       = module.ecr.repository_url
+  image_tag          = var.image_tag
+}
+
+module "dns" {
+  source       = "../../modules/dns"
+  domain_name  = var.domain_name
+  subdomain    = var.subdomain
+  alb_dns_name = module.alb.alb_dns_name
+  alb_zone_id  = module.alb.alb_zone_id
+}
+
+##
---
+++ infra/environments/dev/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "gatus-terraform-state-044260499053"
+    key     = "dev/terraform.tfstate"
+    region  = "eu-west-2"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
