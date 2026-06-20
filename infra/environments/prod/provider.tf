200c0c0dcf917ecf321734e6c7d80ec5512c5f9f
Gus <amuwonge99@gmail.com>
2026-06-20 23:35:30 +0100

    moved backend to separate file

--- infra/environments/prod/provider.tf
+++ infra/environments/prod/provider.tf
@@ -7,13 +7,6 @@ terraform {
       version = "~> 5.0"
     }
   }
-
-  backend "s3" {
-    bucket  = "gatus-terraform-state-044260499053"
-    key     = "prod/terraform.tfstate"
-    region  = "eu-west-2"
-    encrypt = true
-  }
 }
 
 provider "aws" {
