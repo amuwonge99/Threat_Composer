3504ce0ad1c1e1cc5b8180249a14445728cf287d
Gus <amuwonge99@gmail.com>
2026-06-21 02:18:12 +0100

    added cloudflare zone_id & api token

--- infra/environments/prod/variables.tf
+++ infra/environments/prod/variables.tf
@@ -28,3 +28,9 @@ variable "cloudflare_api_token" {
   type        = string
   sensitive   = true
 }
+
+variable "cloudflare_zone_id" {
+  description = "Cloudflare zone ID"
+  type        = string
+}
+
